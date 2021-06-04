bc23589af0ca8b990cb3396e11dcfe083d9d6b25
Tejaswini-Tikariha <success.tejaswini@gmail.com>
2021-06-04 17:56:44 +0530

    sudoku-Game-demo

---
+++ userinterface/IUserInterfaceContract.java
@@ -0,0 +1,19 @@
+package sudoku.userinterface;
+
+import sudoku.problemdomain.SudokuGame;
+
+public interface IUserInterfaceContract {
+    interface EventListener{
+        void onSudokuInput(int x, int y, int input) throws Exception;
+        void onDialogClick() throws Exception;
+    }
+
+    interface View {
+        void setListener(IUserInterfaceContract.EventListener listener);
+        void updateSquare(int x, int y, int input);
+        void updateBoard(SudokuGame game);
+        void showDialog(String Message) throws Exception;
+        void showError(String message);
+
+    }
+}
